1a9e3d3d181b28450e9f135dd1e87056aadac08e
roranger16 <roger.castillonjr@gmail.com>
2023-03-16 15:59:39 +0800

    landing page with footer

--- src/pages/components/about.tsx
+++ src/pages/components/about.tsx
@@ -5,7 +5,7 @@ const About = () => {
         <div className="flex flex-col md:flex-row pt-10 pb-10">
             <div className="w-full md:w-1/2 p-10">
                 <div className="w-full">
-                    <img className="pb-0 h-2xl" src="https://images.unsplash.com/photo-1515378960530-7c0da6231fb1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="programmer working on programming website" />
+                    <img className="pb-0" src="https://images.unsplash.com/photo-1515378960530-7c0da6231fb1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="programmer working on programming website" />
                 </div>
                 {/* <div className="w-1/2  flex flex-wrap md:flex-row"></div> */}
                 <div className="w-full md:flex md:w-1/2 mt-6">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,15 @@ import React from "react";
 import Header from './components/header'
 import Content from './components/hero'
 import About from './components/about'
+import Footer from './components/footer'
 
 const index = () => {
   return (
-    <div className="">
+    <div className="w-100">
       <Header />
       <Content />
       <About />
+      <Footer />
     </div>
   );
 };
