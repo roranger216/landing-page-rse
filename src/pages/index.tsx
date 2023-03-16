96400754b2e6391d9c8b9c7610bbc4c85fe08e7a
changchangGSHV <GrytzhavavesValmoria@gmail.com>
2023-03-16 16:56:10 +0800

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,18 @@
 import React from "react";
-
 import Head from './components/header';
 import Content from './components/hero'
 import About from './components/about'
 import Footer from './components/footer'
 
-const index = () => {
+const Index: React.FC = () => {
   return (
     <div className="">
       <Head />
       <Content />
       <About />
+      <Footer />
     </div>
   );
 };
 
-export default index;
+export default Index;
