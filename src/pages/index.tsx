b5e90e31de82d8172aa6f00defc81a2bacfdbff6
changchangGSHV <GrytzhavavesValmoria@gmail.com>
2023-03-16 16:52:27 +0800

    create footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Head from './components/header';
 import Content from './components/hero'
 import About from './components/about'
+import Footer from './components/footer'
 
 const index = () => {
   return (
