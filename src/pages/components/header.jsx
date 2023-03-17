1e9f9e68d35c28a047c206347bc623a3e384105e
roranger16 <roger.castillonjr@gmail.com>
2023-03-17 16:12:32 +0800

    Navbar with transition

--- src/pages/components/header.jsx
+++ src/pages/components/header.jsx
@@ -37,7 +37,7 @@ const Header = () => {
                     {!nav ? <img src="https://cdn-icons-png.flaticon.com/128/7710/7710488.png" className="" width={24}></img> : <img src="https://cdn-icons-png.flaticon.com/512/1828/1828778.png" className="" width={24}></img>}
                 </div>
                 <div className={!nav ? 'opacity-0' : 'fixed transition-opacity ease-in-out duration-300'}>
-  <ul className="flex flex-col gap-4 fixed left-0 top-16 uppercase p-6 text-center bg-white w-full shadow-2xl opacity-100">
+                    <ul className="flex flex-col gap-4 fixed left-0 top-16 uppercase p-6 text-center bg-white w-full shadow-2xl opacity-100">
                         <li className="text-md font-medium p-2 rounded-md hover:bg-sky-200">
                             <Link to="home" spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>Home</Link>
                         </li>
