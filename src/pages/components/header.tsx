2e7d2d5b56a4a0a4bbfc5911598bc39be258a768
changchangGSHV <GrytzhavavesValmoria@gmail.com>
2023-03-17 11:58:22 +0800

    fix

--- src/pages/components/header.tsx
+++ src/pages/components/header.tsx
@@ -1,79 +1,37 @@
-import React, { useRef,useEffect, CSSProperties } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
-import Link from 'next/link'
+import React, { useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
+const Header = () => {
+  const [nav, setNav] = useState(false);
 
-const Head = () => {
-
-  
-  const sidemenuRef = useRef<HTMLDivElement>(null);
-  const btnMenu = useRef<HTMLDivElement>(null);
-  function showMenu() {
-    if (sidemenuRef.current && btnMenu.current) {
-      const sidebarDisplay = sidemenuRef.current.style.display;
-      sidemenuRef.current.style.display = sidebarDisplay === 'flex' ? 'none' : 'flex';
-        sidemenuRef.current.style.position = 'absolute';
-        sidemenuRef.current.style.marginTop = '80px';
-        sidemenuRef.current.style.backgroundColor = 'rgb(248 250 252)';
-        sidemenuRef.current.style.paddingBottom = '25px';
-    }  
-  }
-
-  useEffect(() => {
-    const handleResize = () => {
-      if(sidemenuRef.current){
-        if (window.innerWidth >= 800) {
-          sidemenuRef.current.style.display = 'none';
-        }
-      }
-    };
-  
-    handleResize();
-  
-    window.addEventListener('resize', handleResize);
-  
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-  
-  
+  const handleNav = () => {
+    setNav(!nav);
+  };
 
   return (
-
-    <div>
-    <div className="flex flex-row justify-between w-full shadow-lg fixed bg-slate-50">
-      <div className="text-center lg:flex-col lg:flex flex justify-between w-full">
-        <div ref={btnMenu} className="items-center justify-between rounded-md p-2 hover:bg-sky-200 ease-in duration-300 min-[900px]:hidden lg:m-6 lg:flex-row m-5">
-          <button id="menu-btn" className='text-2xl' onClick={showMenu}>
-            <FontAwesomeIcon icon={faBars}/>
-          </button>
-        </div>
-        <div className="flex lg:m-6 m-6 font-bold text-xl p-2">THISLogo</div>
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-black
+    '>
+      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+      <ul className='hidden md:flex'>
+        <li className='p-4'>Home</li>
+        <li className='p-4'>Company</li>
+        <li className='p-4'>Resources</li>
+        <li className='p-4'>About</li>
+        <li className='p-4'>Contact</li>
+      </ul>
+      <div onClick={handleNav} className='block md:hidden'>
+          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
       </div>
-      <div className="flex flex-col md:flex-row items-center justify-between lg:gap-20 lg:m-6 font-medium text-lg cursor-pointer" ref={sidemenuRef}>
-        <div id="sidebar" className="hide flex-row flex items-center justify-between gap-20 font-medium text-lg cursor-pointer max-[900px]:hidden">
-          <div className="rounded-md p-2 hover:bg-sky-200 ease-in duration-300"><a href="">Home</a></div>
-          <div className="rounded-md p-2 hover:bg-sky-200 ease-in duration-300"><a href="">About</a></div>
-          <div className="rounded-md p-2 hover:bg-sky-200 ease-in duration-300"><a href="">Services</a></div>
-          <div className="rounded-md p-2 hover:bg-sky-200 ease-in duration-300"><a href="">Contact</a></div>
-          </div>
-      </div>
+      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
+          <li className='p-4 border-b border-gray-600'>Home</li>
+          <li className='p-4 border-b border-gray-600'>Company</li>
+          <li className='p-4 border-b border-gray-600'>Resources</li>
+          <li className='p-4 border-b border-gray-600'>About</li>
+          <li className='p-4'>Contact</li>
+      </ul>
     </div>
-      <div className="hidden w-1/3 pt-px justify-center items-center shadow-lg " ref={sidemenuRef}>
-        <div className="lg:flex lg:flex-col w-1/2 justify-center items-center font-medium text-lg cursor-pointer ">
-          <div className="rounded-md p-2 hover:bg-sky-200 ease-in duration-300">
-            <Link legacyBehavior href='#about'>
-              <a href="">Home</a>
-            </Link></div> 
-          <div className="rounded-md p-2 hover:bg-sky-200 ease-in duration-300"><a href="#about">About</a></div>
-          <div className="rounded-md p-2 hover:bg-sky-200 ease-in duration-300"><a href="">Services</a></div>
-          <div className="rounded-md p-2 hover:bg-sky-200 ease-in duration-300"><a href="">Contact</a></div>
-        </div>
-      </div>
-      </div>
-  )
-}
+  );
+};
 
-export default Head
+export default Header;
--- src/pages/components/search.tsx
+++ src/pages/components/search.tsx
@@ -1,57 +1,9 @@
-import React from 'react';
-import CountUp from 'react-countup';
+import React from 'react'
 
-function Search() {
+function search() {
   return (
-    <div className='grid grid-cols-2 mt-[40px] pb-[40px]'>
-      <div className='text-white'>
-        <div className='pl-[0px]'>
-          <h1 className='text-[48px] leading-[58px] w-[60%] font-bold mt-[50px]'>
-            Your Lightning Fast Delivery Partner
-          </h1>
-          <p className='text-[15px] leading-[23px] font-normal mt-[30px] w-[80%]'>
-            Facere distinctio molestiae nisi fugit tenetur repellat non praesentium nesciunt optio quis sit odio nemo quisquam.
-            eius quos reiciendis eum vel eum voluptatem eum maiores eaque id optio ullam occaecati odio est possimus vel
-            reprehenderit
-          </p>
-          <div className='bg-white w-[60%] pr-[4px] rounded-[6px] flex items-center mt-[30px]'>
-            <input type='text' placeholder='Zip code or City' className='bg-white outline-none w-full text-black py-[17px] pl-[14px]' />
-            <button className='bg-[#2756FF] py-[12px] px-[20px] text-white rounded-[4px]'>Search</button>
-          </div>
-        </div>
-        <div className='grid grid-cols-4 mt-[30px] pl-[20px]'>
-          <div className='space-y-[10px] flex flex-col items-start justify-start'>
-            <h2 className='text-[32px] font-semibold'>
-              <CountUp end={1234} enableScrollSpy />
-            </h2>
-            <div className='h-[4px] w-[20px] bg-[#2756FF]'></div>
-            <p>Clients</p>
-          </div>
-          <div className='space-y-[10px] flex flex-col items-start justify-start'>
-            <h2 className='text-[32px] font-semibold'>
-              <CountUp end={2351} enableScrollSpy />
-            </h2>
-            <div className='h-[4px] w-[20px] bg-[#2756FF]'></div>
-            <p>Customers</p>
-          </div>
-          <div className='space-y-[10px] flex flex-col items-start justify-start'>
-            <h2 className='text-[32px] font-semibold'>
-              <CountUp end={421} enableScrollSpy />
-            </h2>
-            <div className='h-[4px] w-[20px] bg-[#2756FF]'></div>
-            <p>Support</p>
-          </div>
-          <div className='space-y-[10px] flex flex-col items-start justify-start'>
-            <h2 className='text-[32px] font-semibold'>
-              <CountUp end={971} enableScrollSpy />
-            </h2>
-            <div className='h-[4px] w-[20px] bg-[#2756FF]'></div>
-            <p>Workers</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    <div>search</div>
+  )
 }
 
-export default Search;
+export default search
