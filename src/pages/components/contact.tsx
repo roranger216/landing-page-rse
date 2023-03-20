6ffea09e7ce05660de4a8396590bb8fce05c316c
changchangGSHV <GrytzhavavesValmoria@gmail.com>
2023-03-20 09:56:20 +0800

    create contactus from scratch

--- src/pages/components/contact.tsx
+++ src/pages/components/contact.tsx
@@ -1,11 +1,47 @@
+import { Button, TextField } from '@mui/material'
 import React from 'react'
 
 function contact() {
   return (
+    <div>
+    <Card>
+      <CardContent>
+        <form></form>
+
+     <Grid container spacing={1}></Grid>
 
 
     
-    <div>contact</div>
+    </Card>
+    
+     <Grid xs={12} sm={6} item>
+
+     <TextField label="First Name" placeholder= "Enter firstname" variant="outlined" fullWidth required/>
+     </Grid>
+
+    <Grid xs={12} sm={6} item>
+
+    <TextField label="Last Name" placeholder= "Enter lastname" variant="outlined" fullWidth required/>
+    </Grid>
+    <Grid xs={12} item>
+    <TextField type="email" label="Email" placeholder= "Enter your email" variant="outlined" fullWidth required/>
+    </Grid>
+
+    <Grid xs={12} item>
+    <TextField type="number" label="Number" placeholder= "Enter your phone number" variant="outlined" fullWidth required/>
+    </Grid>
+    <Grid xs={12} item>
+    <TextField label="Message" multiline rows={6} placeholder= "Type your message here" variant="outlined" fullWidth required/>
+    </Grid>
+    <Grid xs={12} item>
+    <Button type="submit" variant="contained" color="primary" fullWidth>Submit</Button>
+    </Grid>
+
+    </form>
+    </CardContent>
+    </Card>
+
+    </div>
   )
 }
 
