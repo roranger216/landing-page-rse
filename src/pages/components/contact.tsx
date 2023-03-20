c5bc9106d31f0c672e7fc219ea89a71fe85e6a7b
changchangGSHV <GrytzhavavesValmoria@gmail.com>
2023-03-20 10:08:38 +0800

    fix bugs

--- src/pages/components/contact.tsx
+++ src/pages/components/contact.tsx
@@ -1,48 +1,49 @@
-import { Button, TextField } from '@mui/material'
-import React from 'react'
+import { Button, TextField, Card, CardContent, Grid } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import React from 'react';
 
-function contact() {
+function Contact() {
   return (
     <div>
-    <Card>
-      <CardContent>
-        <form></form>
+      <Card style={{ maxWidth: 450, margin: '0 auto', padding: '20px' }}>
+        <CardContent>
+          <Typography gutterBottom variant="h5">
+            CONTACT US
+          </Typography>
+          <Typography gutterBottom variant="body2">
+            Greetings of Peace! Fill out the form and our team will get back to you. Thank you!
+          </Typography>
+          <form>
+            <Grid container spacing={1}>
+              <Grid xs={12} sm={6} item>
+                <TextField label="First Name" placeholder="Enter firstname" variant="outlined" fullWidth required />
+              </Grid>
 
-     <Grid container spacing={1}></Grid>
+              <Grid xs={12} sm={6} item>
+                <TextField label="Last Name" placeholder="Enter lastname" variant="outlined" fullWidth required />
+              </Grid>
 
+              <Grid xs={12} item>
+                <TextField type="email" label="Email" placeholder="Enter your email" variant="outlined" fullWidth required />
+              </Grid>
 
-    
-    </Card>
-    
-     <Grid xs={12} sm={6} item>
+              <Grid xs={12} item>
+                <TextField type="number" label="Number" placeholder="Enter your phone number" variant="outlined" fullWidth required />
+              </Grid>
 
-     <TextField label="First Name" placeholder= "Enter firstname" variant="outlined" fullWidth required/>
-     </Grid>
+              <Grid xs={12} item>
+                <TextField label="Message" multiline rows={6} placeholder="Type your message here" variant="outlined" fullWidth required />
+              </Grid>
 
-    <Grid xs={12} sm={6} item>
-
-    <TextField label="Last Name" placeholder= "Enter lastname" variant="outlined" fullWidth required/>
-    </Grid>
-    <Grid xs={12} item>
-    <TextField type="email" label="Email" placeholder= "Enter your email" variant="outlined" fullWidth required/>
-    </Grid>
-
-    <Grid xs={12} item>
-    <TextField type="number" label="Number" placeholder= "Enter your phone number" variant="outlined" fullWidth required/>
-    </Grid>
-    <Grid xs={12} item>
-    <TextField label="Message" multiline rows={6} placeholder= "Type your message here" variant="outlined" fullWidth required/>
-    </Grid>
-    <Grid xs={12} item>
-    <Button type="submit" variant="contained" color="primary" fullWidth>Submit</Button>
-    </Grid>
-
-    </form>
-    </CardContent>
-    </Card>
-
+              <Grid xs={12} item>
+                <Button type="submit" variant="contained" color="primary" fullWidth>Submit</Button>
+              </Grid>
+            </Grid>
+          </form>
+        </CardContent>
+      </Card>
     </div>
-  )
+  );
 }
 
-export default contact
+export default Contact;
