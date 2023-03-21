e1931e4f5bab8728a0f62cbe469e09fcf1fa9c66
roranger16 <roger.castillonjr@gmail.com>
2023-03-21 09:18:09 +0800

    pleasant design 2

--- src/pages/components/hero.tsx
+++ src/pages/components/hero.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Content = () => {
     return (
-        <div id="home" className="flex flex-col lg:flex-row justify-between items-center pt-32 md:pt-10 lg:pt-40 pb-20 px-4 lg:px-0 m-4 md:m-20">
+        <div id="home" className="flex flex-col lg:flex-row justify-between items-center pt-32 md:pt-10 lg:pt-40 xl:pt-20 pb-20 px-4 lg:px-0 m-4 md:m-20">
             {/* <div className="absolute bottom-0 left-0 w-full h-1/6 bg-sky-300 z-1"></div> */}
             <div className="text-center lg:text-left lg:w-1/2 lg:pr-10">
                 <h3 className="text-3xl lg:text-3xl font-medium">Efficient and Comprehensive</h3>
@@ -13,7 +13,7 @@ const Content = () => {
                 </button>
             </div>
             <div className="lg:w-1/2">
-                <img className="w-full rounded-lg" src="https://amincodes.com/html/one-pages/flex-it/assets/images/hero/illustration-1.png" />
+                <img className="w-full" src="https://amincodes.com/html/one-pages/flex-it/assets/images/hero/illustration-1.png" />
             </div>
         </div>
     )
