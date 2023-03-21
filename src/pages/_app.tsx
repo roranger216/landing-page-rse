d829113d751ad3ec519efe600db23a507bf75d3e
changchangGSHV <GrytzhavavesValmoria@gmail.com>
2023-03-21 09:33:59 +0800

    create contactus trial

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import { AppProps } from "next/app";
 import "../assets/styles/global.css";
-function MyApp({ Component, pageProps }) {
-  return (
-  );
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
