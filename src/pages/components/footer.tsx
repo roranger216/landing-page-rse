f5fa33febb4c2985e553c06f717cb37d018fe78a
roranger16 <roger.castillonjr@gmail.com>
2023-03-21 14:23:53 +0800

    fix footer from light to dark

--- src/pages/components/footer.tsx
+++ src/pages/components/footer.tsx
@@ -3,11 +3,11 @@ import React from "react";
 const Footer = () => {
 	return (
 		<div className="w-full absolute left-0">
-			<div className="bg-gray-200 h-1/2 flex md:flex-row flex-col justify-around items-start p-20">
+			<div className="bg-neutral-800 flex md:flex-row flex-col justify-around items-start p-20">
 				<div className="p-5 ">
 					<ul>
-						<p className="text-gray-800 font-bold text-3xl pb-6">
-							THIS<span className="text-blue-600">Logo</span>
+						<p className="text-white font-bold text-3xl pb-6">
+							THIS<span className="text-sky-600">Logo</span>
 						</p>
 						<div className="flex gap-6 pb-5">
                             <img src="https://cdn-icons-png.flaticon.com/512/3955/3955024.png" width={28}></img>
@@ -19,64 +19,64 @@ const Footer = () => {
 				</div>
 				<div className="p-5">
 					<ul>
-						<p className="text-gray-800 font-bold text-2xl pb-4">Services</p>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+						<p className="text-gray-100 font-bold text-2xl pb-4">Services</p>
+						<li className="text-gray-400 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
 							Stocks
 						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+						<li className="text-gray-400 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
 							Futures & Options
 						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+						<li className="text-gray-400 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
 							Mutual Funds
 						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+						<li className="text-gray-400 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
 							Fixed deposits
 						</li>
 					</ul>
 				</div>
 				<div className="p-5">
 					<ul>
-						<p className="text-gray-800 font-bold text-2xl pb-4">Company</p>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+						<p className="text-gray-100 font-bold text-2xl pb-4">Company</p>
+						<li className="text-gray-400 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
 							About
 						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+						<li className="text-gray-400 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
 							Products
 						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+						<li className="text-gray-400 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
 							Pricing
 						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+						<li className="text-gray-400 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
 							Careers
 						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+						<li className="text-gray-400 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
 							Press & Media
 						</li>
 					</ul>
 				</div>
 				<div className="p-5">
 					<ul>
-						<p className="text-gray-800 font-bold text-2xl pb-4">Support</p>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+						<p className="text-gray-100 font-bold text-2xl pb-4">Support</p>
+						<li className="text-gray-400 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
 							Contact
 						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+						<li className="text-gray-400 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
 							Support Portals
 						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+						<li className="text-gray-400 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
 							List Of Charges
 						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+						<li className="text-gray-400 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
 							Downloads & Resources
 						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+						<li className="text-gray-400 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
 							Videos
 						</li>
 					</ul>
 				</div>
 			</div>
-			<div className="flex flex-col justify-center items-center text-center  p-5 bg-gray-200">
-				<h1 className=" text-gray-800 font-semibold">
+			<div className="flex flex-col justify-center items-center text-center  p-5 bg-neutral-800">
+				<h1 className=" text-gray-400 font-semibold">
 					© 2022-2023 All rights reserved | Build with ❤ by{" "}
 					<span className="hover:text-blue-600 font-semibold cursor-pointer">
 						Team RSE INTERNS{" "}
