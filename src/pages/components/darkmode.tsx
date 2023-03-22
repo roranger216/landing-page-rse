23a5eab1202399326dad47e6dc0a1acab93f452f
changchangGSHV <GrytzhavavesValmoria@gmail.com>
2023-03-22 13:54:02 +0800

    create darkmode

---
+++ src/pages/components/darkmode.tsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { useTheme } from 'next-themes';
+import { FaMoon } from 'react-icons/fa';
+import { FaSun} from 'react-icons/fa'
+
+
+const Darkmode = () => {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const renderThemeChanger = () => {
+    if (!mounted) return null;
+
+    const currentTheme = theme === 'system' ? systemTheme : theme;
+
+    if (currentTheme === 'dark') {
+      return (
+        <FaSun
+          className="w-7 h-7"
+          role="button"
+          onClick={() => setTheme('light')}
+        />
+      );
+    } else {
+      return (
+        <FaMoon
+          className="w-7 h-7"
+          role="button"
+          onClick={() => setTheme('dark')}
+        />
+      );
+    }
+  };
+
+  return (
+    <footer className="border-b border-gray-100 dark:border-gray-700">
+      <div className="container mx-auto px-4 sm:px-6 py-4 flex justify-between items-center">
+        {renderThemeChanger()}
+      </div>
+    </footer>
+  );
+};
+
+export default Darkmode;
