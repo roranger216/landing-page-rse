e79e87c1b7278244601ecef81e90f0977fee6629
CodingKnight08 <macedajayson11@gmail.com>
2023-03-22 10:06:11 +0800

    create sign-in function

--- src/pages/components/signIn.tsx
+++ src/pages/components/signIn.tsx
@@ -71,14 +71,14 @@ const SignIn = () => {
             </div>
             <div className="inputs flex justify-center">
                 <div className="flex w-full flex-col justify-center items-center gap-6">
-                    <input type="email" name="username" placeholder="Email" className="w-3/4 h-1/3 text-justify px-2 rounded-md border-2 border-blue-300 p-5" />
-                    <input type="password" name="password" placeholder="Password" className="w-3/4 h-1/3 text-justify px-2 rounded-md border-2 border-blue-300 p-5" />
+                    <input value={emailValue} onChange={handleEmail}type="email" name="username" placeholder="Email" className="w-3/4 h-1/3 text-justify px-2 rounded-md border-2 border-blue-300 p-5" />
+                    <input value={passwordValue} onChange={handlePassword} type="password" name="password" placeholder="Password" className="w-3/4 h-1/3 text-justify px-2 rounded-md border-2 border-blue-300 p-5" />
                 </div>
             </div>
             <div className="buttons w-full">
                 <div className="flex flex-col items-center justify-between mb-20">
-                    <button type="submit" className=" w-2/5 bg-blue-600 hover:bg-blue-400 p-1 rounded-md font-bold text-lg text-white hover:text-gray-200">
-                        Sign in
+                    <button onClick= {handleButtonClick} type="submit" className=" w-2/5 bg-blue-600 hover:bg-blue-400 p-1 rounded-md font-bold text-lg text-white hover:text-gray-200">
+                        Sign Up
                     </button>
                     <div className="flex flex-col items-center">
                         <p className="flex flex-row max-[900px]:flex-col justify-center items-center">Don&apos;t have an account? <a href="" className='text-blue-600'>create account</a></p>
