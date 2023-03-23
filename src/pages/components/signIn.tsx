8f8143b3f251f37cedbbcc1db47f65ace1ee69f2
changchangGSHV <GrytzhavavesValmoria@gmail.com>
2023-03-23 17:13:06 +0800

    signIn update

--- src/pages/components/signIn.tsx
+++ src/pages/components/signIn.tsx
@@ -6,7 +6,9 @@ const SignIn = () => {
 
     const [accountArray, setAccountArray] = useState([
         {id:1, email:'jayson@gmail.com', password:'123456'},
-        {id:2, email:'dsteph@gmail.com',password:'hey'}
+        {id:2, email:'dsteph@gmail.com',password:'hey'},
+        {id:3, email:'roger@gmail.com', password:'sarge'},
+        {id:4, email:'grytz@gmail.com', password:'chang'},
     ]);
 
 
