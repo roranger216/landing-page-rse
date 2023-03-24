97c15731ad909ccdca3ac1517e3b86884ce5c5a5
changchangGSHV <GrytzhavavesValmoria@gmail.com>
2023-03-24 14:09:57 +0800

    signIn updates

--- src/pages/components/auth/signin.tsx
+++ src/pages/components/auth/signin.tsx
@@ -12,7 +12,9 @@ const SignIn = () => {
   const accountArray =[
     { id: 1, email: 'jayson@gmail.com', password: '123456' },
     { id: 2, email: 'dsteph@gmail.com', password: 'hey' },
-    { id: 3, email: 'changz@gmail.com', password: 'zzz' },
+    { id: 3, email: 'roger@gmail.com', password: 'jjj' },
+    { id: 4, email: 'grytz@gmail.com', password: 'chang' },
+    
   ];
 
 
