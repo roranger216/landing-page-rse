18a7faf0fd47c1d7e7f6510cd9368b92b26f975b
steeeeeeeep <stephenedave.dorado@msugensan.edu.ph>
2023-03-27 14:26:16 +0800

    integrating services.tsx

--- src/pages/main/homepage.tsx
+++ src/pages/main/homepage.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import Header from './dashboard_header'
+import Header from './template'
+import Footer from './footer';
 
 const Homepage = () => {
   return (
@@ -9,55 +10,8 @@ const Homepage = () => {
       </nav>
 
       <div className="mt-16 overflow-hidden ">
-        <div className="row w-screen p-8 rounded-lg bg-gray-100">
-          <div className="flex flex-col justify-center md:ml-60 bg-white p-8 rounded-md">
-            <div className="Logo flex justify-center items-center">
-              <div id="service_logo" className="flex flex-row flex-wrap w-full justify-center">
-                <div id="logo1" className='md:w-28 rounded-full p-4'>
-                  <img src="https://cdn-icons-png.flaticon.com/512/5431/5431676.png" alt="development" className='md:w-24' />
-                </div>
-              </div>
-              <div id="service_logo" className="flex flex-row flex-wrap rounded-md w-full justify-center">
-                <div id="logo2" className='md:w-28 rounded-full p-4'>
-                  <img src="https://cdn-icons-png.flaticon.com/512/1352/1352837.png" alt="customization" className='md:w-24' />
-                </div>
-              </div>
-              <div id="service_logo" className="flex flex-row flex-wrap rounded-md w-full justify-center">
-                <div id="logo1" className='md:w-28 rounded-full p-4'>
-                  <img src="https://cdn-icons-png.flaticon.com/512/2961/2961226.png" alt="consultation" className='md:w-24' />
-                </div>
-              </div>
-              <div id="service_logo" className="flex flex-row flex-wrap rounded-md w-full justify-center">
-                <div id="logo2" className='md:w-28 rounded-full p-4'>
-                  <img src="https://cdn-icons-png.flaticon.com/512/4878/4878520.png" alt="documentation" className='md:w-24' />
-                </div>
-              </div>
-              <div id="service_logo" className="flex flex-row flex-wrap rounded-md w-full justify-center">
-                <div id="logo1" className='md:w-28 rounded-full p-4'>
-                  <img src="https://cdn-icons-png.flaticon.com/512/922/922699.png" alt="design" className='md:w-24' />
-                </div>
-              </div>
-              <div id="service_logo" className="flex flex-row flex-wrap rounded-md w-full justify-center">
-                <div id="logo2" className='md:w-28 rounded-full p-4'>
-                  <img src="https://cdn-icons-png.flaticon.com/512/6095/6095494.png" alt="It Works" className='md:w-24' />
-                </div>
-              </div>
-            </div>
-            <div className="Logo flex justify-center items-center mt-10">
-              <h1 className="font-bold text-3xl mb-8">
-                THIS<span className="text-blue-600">Logo</span>
-              </h1>
-            </div>
-            <div className="Logo flex justify-center items-center">
-              <h1 className="font-bold text-3xl mb-8">
-                THIS<span className="text-blue-600">Logo</span>
-              </h1>
-            </div>
-            <div className="Logo flex justify-center items-center">
-              <h1 className="font-bold text-3xl mb-8">
-                THIS<span className="text-blue-600">Logo</span>
-              </h1>
-            </div>
+        <div className="row w-screen">
+          <div className="flex flex-col justify-center md:ml-60 ">
             <div className="Logo flex justify-center items-center">
               <h1 className="font-bold text-3xl mb-8">
                 THIS<span className="text-blue-600">Logo</span>
@@ -97,16 +51,9 @@ const Homepage = () => {
         </div>
       </div>
       <div className="-z-10">
-        <div className="flex flex-row justify-center items-center text-center md:ml-60 overflow-hidden bg-gray-800">
-          <h1 className=" text-gray-400 md:font-semibold">
-            © 2022-2023 All rights reserved | Build with ❤ by{" "}
-            <span className="hover:text-blue-600 md:font-semibold cursor-pointer">
-              Team RSE INTERNS{" "}
-            </span>
-          </h1>
-        </div>
+        <Footer />
       </div>
-
+      
     </div>
   )
 }
